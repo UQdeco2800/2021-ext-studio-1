b715dcbd3b1ab7ac615bc66bd1b416cbd9bdc3db
Abstrusei <smarshallean@gmail.com>
2021-08-29 03:49:53 +1000

    Fixed issue where 'exit' button wouldn't appear on the main game screen. This was done by altering RagnorakRacer.java

--- source/core/src/main/com/deco2800/game/screens/RagnorakRacer.java
+++ source/core/src/main/com/deco2800/game/screens/RagnorakRacer.java
@@ -2,17 +2,26 @@ package com.deco2800.game.screens;
 
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.deco2800.game.GdxGame;
 import com.deco2800.game.areas.RainbowBridge;
 import com.deco2800.game.areas.terrain.TerrainFactory;
 import com.deco2800.game.components.bridge.Bridge;
+import com.deco2800.game.components.gamearea.PerformanceDisplay;
+import com.deco2800.game.components.maingame.MainGameActions;
+import com.deco2800.game.components.maingame.MainGameExitDisplay;
+import com.deco2800.game.entities.Entity;
 import com.deco2800.game.entities.EntityService;
 import com.deco2800.game.entities.factories.RenderFactory;
+import com.deco2800.game.input.InputComponent;
+import com.deco2800.game.input.InputDecorator;
 import com.deco2800.game.input.InputService;
 import com.deco2800.game.rendering.RenderService;
 import com.deco2800.game.rendering.Renderer;
 import com.deco2800.game.services.ResourceService;
 import com.deco2800.game.services.ServiceLocator;
+import com.deco2800.game.ui.terminal.Terminal;
+import com.deco2800.game.ui.terminal.TerminalDisplay;
 
 public class RagnorakRacer extends ScreenAdapter {
 
@@ -32,6 +41,8 @@ public class RagnorakRacer extends ScreenAdapter {
 
         this.renderer = RenderFactory.createRenderer();
         this.renderer.getCamera().getEntity().setPosition(CAMERA_POSITION);
+
+        createUI();
 
         TerrainFactory terrainFactory = new TerrainFactory(renderer.getCamera());
         RainbowBridge rainbowBridge = new RainbowBridge(terrainFactory);
@@ -73,5 +84,26 @@ public class RagnorakRacer extends ScreenAdapter {
     @Override
     public void dispose() {
         super.dispose();
+    }
+
+    /**
+     * Creates the main game's ui including components for rendering ui elements to the screen and
+     * capturing and handling ui input.
+     */
+    private void createUI() {
+        Stage stage = ServiceLocator.getRenderService().getStage();
+        InputComponent inputComponent =
+                ServiceLocator.getInputService().getInputFactory().createForTerminal();
+
+        Entity ui = new Entity();
+        ui.addComponent(new InputDecorator(stage, 10))
+                .addComponent(new PerformanceDisplay())
+                .addComponent(new MainGameActions(this.game))
+                .addComponent(new MainGameExitDisplay())
+                .addComponent(new Terminal())
+                .addComponent(inputComponent)
+                .addComponent(new TerminalDisplay());
+
+        ServiceLocator.getEntityService().register(ui);
     }
 }
