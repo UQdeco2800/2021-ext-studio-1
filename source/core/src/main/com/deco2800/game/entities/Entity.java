6e65d5eae42e84168063092dbc4f8e0868c20358
JiashengLi <88020895+JiashengLi@users.noreply.github.com>
2021-09-13 11:20:35 +0800

    Add new types of entities:Weapons,Collectables

--- source/core/src/main/com/deco2800/game/entities/Entity.java
+++ source/core/src/main/com/deco2800/game/entities/Entity.java
@@ -55,7 +55,7 @@ public class Entity {
   }
 
   public enum Type {
-    PLAYER, GHOST, OBSTACLE, GHOSTKING
+    PLAYER, GHOST, OBSTACLE, GHOSTKING,WEAPON,COLLECTABLES
   }
 
   public Entity.Type getType() {
