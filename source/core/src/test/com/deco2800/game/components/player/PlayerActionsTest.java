203d7ff5177b102c68c0c39fd15cde9ae427e5a2
Vermouth10 <972209899@qq.com>
2021-10-02 23:34:09 +0800

    Code of test of PlayerActions

--- source/core/src/test/com/deco2800/game/components/player/PlayerActionsTest.java
+++ source/core/src/test/com/deco2800/game/components/player/PlayerActionsTest.java
@@ -4,8 +4,8 @@ import com.badlogic.gdx.utils.Array;
 import com.deco2800.game.entities.Entity;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PlayerActionsTest {
     private PlayerActions playerActions;
@@ -22,7 +22,7 @@ public class PlayerActionsTest {
     private Array<Entity> entities4 = new Array<>(1);
 
     @Test
-    public void findNearestTargetsTest1() throws NullPointerException{
+    public void findNearestTargetsTest1() {
         float minDstEnemy = 1.8f;
         float dst = 0;
         Entity entityFind = new Entity();
@@ -37,5 +37,131 @@ public class PlayerActionsTest {
         }
         assertTrue(minDstEnemy > dst);
         assertEquals("GHOST", entityFind.getType().toString());
+    }
+
+    @Test
+    public void findNearestTargetsTest2() {
+        float minDstEnemy = 1.8f;
+        float dst = 0;
+        Entity entityFind = new Entity();
+        entity.setPosition(0, 0);
+        entity1.setPosition(2, 0);
+        entities1.add(entity1);
+        for (Entity en : entities1) {
+            dst = entity.getPosition().dst(en.getPosition());
+            if (minDstEnemy > dst) {
+                entityFind = en;
+            }
+        }
+        assertFalse(minDstEnemy > dst);
+        assertNull(entityFind.getType());
+    }
+
+    @Test
+    public void findNearestTargetsTest3() {
+        float minDstEnemy = 1.8f;
+        float dst = 0;
+        Entity entityFind = new Entity();
+        entity.setPosition(0, 0);
+        entity2.setPosition(1, 0);
+        entities2.add(entity2);
+        for (Entity en : entities2) {
+            dst = entity.getPosition().dst(en.getPosition());
+            if (minDstEnemy > dst) {
+                entityFind = en;
+            }
+        }
+        assertTrue(minDstEnemy > dst);
+        assertEquals("GHOSTKING", entityFind.getType().toString());
+    }
+
+    @Test
+    public void findNearestTargetsTest4() {
+        float minDstEnemy = 1.8f;
+        float dst = 0;
+        Entity entityFind = new Entity();
+        entity.setPosition(0, 0);
+        entity2.setPosition(8, 0);
+        entities2.add(entity2);
+        for (Entity en : entities2) {
+            dst = entity.getPosition().dst(en.getPosition());
+            if (minDstEnemy > dst) {
+                entityFind = en;
+            }
+        }
+        assertFalse(minDstEnemy > dst);
+        assertNull(entityFind.getType());
+    }
+
+    @Test
+    public void findNearestTargetsTest5() {
+        float minDstObstacle = 1.8f;
+        float dst = 0;
+        Entity entityFind = new Entity();
+        entity.setPosition(0, 0);
+        entity3.setPosition(1, 0);
+        entities3.add(entity3);
+        for (Entity en : entities3) {
+            dst = entity.getPosition().dst(en.getPosition());
+            if (minDstObstacle > dst) {
+                entityFind = en;
+            }
+        }
+        assertTrue(minDstObstacle > dst);
+        assertEquals("OBSTACLE", entityFind.getType().toString());
+    }
+
+    @Test
+    public void findNearestTargetsTest6() {
+        float minDstObstacle = 1.8f;
+        float dst = 0;
+        Entity entityFind = new Entity();
+        entity.setPosition(0, 0);
+        entity3.setPosition(5, 0);
+        entities3.add(entity3);
+        for (Entity en : entities3) {
+            dst = entity.getPosition().dst(en.getPosition());
+            if (minDstObstacle > dst) {
+                entityFind = en;
+            }
+        }
+        assertFalse(minDstObstacle > dst);
+        assertNull(entityFind.getType());
+    }
+
+    @Test
+    public void findNearestTargetsTest7() {
+        float minDstObstacle = 1.8f;
+        float dst = 0;
+        Entity entityFind = new Entity();
+        entity.setPosition(0, 0);
+        entity4.setPosition(1, 0);
+        entities4.add(entity4);
+        for (Entity en : entities4) {
+            dst = entity.getPosition().dst(en.getPosition());
+            if (minDstObstacle > dst) {
+                entityFind = en;
+            }
+        }
+        assertTrue(minDstObstacle > dst);
+        assertEquals("BUFF", entityFind.getType().toString());
+    }
+
+    @Test
+    public void findNearestTargetsTest8() {
+        float minDstObstacle = 1.8f;
+        float dst = 0;
+        Entity entityFind = new Entity();
+        entity.setPosition(0, 0);
+        entity4.setPosition(4, 0);
+        entities4.add(entity4);
+        for (Entity en : entities4) {
+            dst = entity.getPosition().dst(en.getPosition());
+            if (minDstObstacle > dst) {
+                entityFind = en;
+            }
+        }
+        assertFalse(minDstObstacle > dst);
+        assertNull(entityFind.getType());
     }
 }
