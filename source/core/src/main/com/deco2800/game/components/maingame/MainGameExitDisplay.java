a03d7d3f15903d60c7ce8829dd9e0bff084997dd
Aayush Aggarwal <college.aayush@gmail.com>
2021-10-03 01:03:39 +0530

    Changed Exit button on Main Menu Screen and Main Game Screen
    Made new exit button to maintain the design consistency of the game

--- source/core/src/main/com/deco2800/game/components/maingame/MainGameExitDisplay.java
+++ source/core/src/main/com/deco2800/game/components/maingame/MainGameExitDisplay.java
@@ -50,9 +50,9 @@ public class MainGameExitDisplay extends UIComponent {
    //Creates exit button
     Button.ButtonStyle exitStyle = new Button.ButtonStyle();
     exitStyle.up= new TextureRegionDrawable(new TextureRegion(
-            new Texture(Gdx.files.internal("images/btn_exit1.png"))));
+            new Texture(Gdx.files.internal("images/NewBtn_exit1.png"))));
     exitStyle.over= new TextureRegionDrawable(new TextureRegion(
-            new Texture(Gdx.files.internal("images/btn_exit2.png"))));
+            new Texture(Gdx.files.internal("images/NewBtn_exit2.png"))));
     Button mainMenuBtn = new Button(exitStyle);
 
     // Triggers an event when the button is pressed.
