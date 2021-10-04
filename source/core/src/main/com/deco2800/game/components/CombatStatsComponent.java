bad370f4860fcdd8ce2f474dac7aedd9b678e1f1
AdrianLSY <sites.ad.p3@gmail.com>
2021-10-05 02:14:27 +0800

    removed additional code snippet

--- source/core/src/main/com/deco2800/game/components/CombatStatsComponent.java
+++ source/core/src/main/com/deco2800/game/components/CombatStatsComponent.java
@@ -146,8 +146,10 @@ public class CombatStatsComponent extends Component {
         AnimationRenderComponent2 animator =
                 attacker.getEntity().getComponent(AnimationRenderComponent2.class);
         animator.startAnimation("touch");
-        Sound attackSound = ServiceLocator.getResourceService().getAsset("sounds/e.ogg", Sound.class);
-        attackSound.play();
+
+//        Asset not loading causing crashes - @adrian Team 4
+//        Sound attackSound = ServiceLocator.getResourceService().getAsset("sounds/e.ogg", Sound.class);
+//        attackSound.play();
         logger.error("--end--attacker--{}",attacker.getEntity().getType());
 
       }
@@ -182,7 +184,7 @@ public class CombatStatsComponent extends Component {
         AnimationRenderComponent3 animator =
                 attacker.getEntity().getComponent(AnimationRenderComponent3.class);
         animator.startAnimation("buff");
-        
+
 //        Asset not loading causing crashes - @adrian Team 4
 //        Sound attackSound = ServiceLocator.getResourceService().getAsset(
 //                "sounds/buff_recover.ogg", Sound.class);
