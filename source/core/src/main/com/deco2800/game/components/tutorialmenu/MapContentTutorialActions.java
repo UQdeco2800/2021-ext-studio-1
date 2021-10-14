269e0b39d4d79967d523784e67a2e01794471bf4
Aayush Aggarwal <college.aayush@gmail.com>
2021-10-14 16:24:09 +0530

    Created MapContentTutorialActions.java

---
+++ source/core/src/main/com/deco2800/game/components/tutorialmenu/MapContentTutorialActions.java
@@ -0,0 +1,51 @@
+package com.deco2800.game.components.tutorialmenu;
+
+import com.deco2800.game.GdxGame;
+import com.deco2800.game.components.Component;
+import com.deco2800.game.services.ServiceLocator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.deco2800.game.GdxGame.ScreenType;
+//import com.deco2800.game.components.tutorialmenu;
+
+/**
+ * This class listens to events relevant to the Main Menu Screen and does something when one of the
+ * events is triggered.
+ */
+public class MapContentTutorialActions extends Component {
+    private static final Logger logger = LoggerFactory.getLogger(MapContentTutorialActions.class);
+    private GdxGame game;
+
+    public MapContentTutorialActions(GdxGame game) {
+        this.game = game;
+    }
+
+    @Override
+    public void create() {
+        entity.getEvents().addListener("exit", this::onExit);
+        entity.getEvents().addListener("EnemyTutorial", this::onEnemyTutorial);
+
+    }
+
+    private void exitMenu() {
+        game.setScreen(ScreenType.MAIN_MENU);
+    }
+
+    /**
+     * Swaps to Enemy Tutorial Screen.
+     */
+    private void onEnemyTutorial() {
+        logger.info("Launching map content tutorial screen");
+        game.setScreen(GdxGame.ScreenType.ENEMY_TUTORIAL);
+    }
+
+    /**
+     * Exits the game.
+     */
+    private void onExit() {
+        logger.info("Exit game");
+        logger.info("Launch enemy tutorial screen");
+        exitMenu();
+    }
+
+}
