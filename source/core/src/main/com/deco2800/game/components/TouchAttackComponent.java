ad3bf378ff85270605e057fd8ad57c57ba4fb494
manavvbahl <s4651601@student.uq.edu.au>
2021-10-19 05:16:49 +0530

    Removed bugs from sonar cloud

--- source/core/src/main/com/deco2800/game/components/TouchAttackComponent.java
+++ source/core/src/main/com/deco2800/game/components/TouchAttackComponent.java
@@ -8,7 +8,6 @@ import com.deco2800.game.physics.BodyUserData;
 import com.deco2800.game.physics.PhysicsLayer;
 import com.deco2800.game.physics.components.HitboxComponent;
 import com.deco2800.game.physics.components.PhysicsComponent;
-import com.deco2800.game.components.player.InventoryComponent;
 
 /**
  * When this entity touches a valid enemy's hitbox, deal damage to them and apply a knockback.
@@ -67,11 +66,6 @@ public class TouchAttackComponent extends Component {
       // Doesn't match our target layer, ignore
       return;
     }
-
-//    if (!PhysicsLayer.contains(targetLayer, other.getFilterData().categoryBits)) {
-//      // Doesn't match our target layer, ignore
-//      return;
-//    }
 
     // Try to attack target.
     Entity target = ((BodyUserData) other.getBody().getUserData()).entity;
